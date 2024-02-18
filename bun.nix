4decbd80eb263694662e3deda623d02f3b5305e9
firesquid6 <jdeiss06@gmail.com>
2024-02-17 22:08:28 -0600

    fix bun.nix

--- bun.nix
+++ bun.nix
@@ -1,13 +1,16 @@
-# If you're a non-nix user, you'll need:
-# - bun (whatever the latest version is)
-# - node js version 20
-# - flyctl (if you plan on deploying to fly.io)
-# - libgcc is just there because some neovim plugins need it
+# Help! I'm a nix user and it isn't working!
+#
+# try running these commands:
+# sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
+# sudo nix-channel --update
+# then run nix-shell again
+
 let
-  unstable = import (fetchTarball https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz) { };
+  unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
 in
 { nixpkgs ? import <nixpkgs> { } }:
 with nixpkgs; mkShell {
+  DOCKER_BUILDKIT = "1";
   buildInputs = [
     unstable.bun
     nodejs_20
